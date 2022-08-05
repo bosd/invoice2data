4e6d08b8c2377b8c9aedba38be7f00094460888f
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2022-08-05 18:49:51 +0200

    import json rules

--- src/invoice2data/extract/plugins/camelot.py
+++ src/invoice2data/extract/plugins/camelot.py
@@ -3,8 +3,20 @@
 import re
 import logging
 import json
+import numpy as np
+
+
+
+# PYTHONIOENCODIG="utf-8" # python -c 'import sys'
+
+# to check is pandas available?
+# import pandas as pd
+
+# PYTHONIOENCODING="UTF-8"
 
 logger = logging.getLogger(__name__)
+
+logging.getLogger("pdfminer").setLevel(logging.WARNING)
 
 DEFAULT_OPTIONS = {"field_separator": r"\s+", "line_separator": r"\n"}
 
@@ -16,6 +28,7 @@ DEFAULT_OPTIONS = {"field_separator": r"\s+", "line_separator": r"\n"}
 # parser seems more logical? but also no path
 '''
 
+# invoice2data camelot-example.pdf --input-reader=pdftotext --template-folder=./Templates --debug
 
 def extract(self, content, output, invoicefile):
     # does not include path in plugin folder
@@ -49,7 +62,8 @@ def extract(self, content, output, invoicefile):
     # temp to show what is stored in self
     logger.debug(self)
     logger.debug("%s is the invoicefile",invoicefile)  # is there a path var?
-    logger.debug("Camelot :)")
+    # logger.debug("Camelot :)")
+
     # allows to pass multiple camelot arguments
     for setting in self["camelot"]:
 
@@ -59,26 +73,50 @@ def extract(self, content, output, invoicefile):
         assert "start" in setting, "Start setting for Camelot missing"
         assert "end" in setting, "End setting for Camelot missing"
         assert "body" in setting, "Body setting missing"
+        #assert (
+        #    value.count(self.options["decimal_separator"]) < 2
+        #), "Decimal separator cannot be present several times"
 
         # Which inputs are needed??
         # Settings json for camelot
         # Which table number to extract?? (Should we do this what if recognition issues?)
         # if passed, do only that table, else do all
-        currency_symbol = "€"
+
+        # can/should be handled in invoice_template.py or here as pandas is quicker
+        # see parse_number
 
         ######################
         # Options
         ######################
         # Change the column headers to a key from the config file
-        # Replace the currency sign
-        # Replace custom characters, like % sign for vat
         # set datatype?
         # Option: Use regex, for example start_date parsing
+
+        # Automatically remove the currency sign when an amount field is set.
+
+        # SECTION HEADERS, TITLE's
+        # ho to mark a record with off standard settings? for example, section headers?
+        # regularly it will be converted to json with the column header as key.
+        # we want to replace that key if:
+        # Regex matches
+        # The columns besid it are empty.
+        # eg no price info or qty means it is a section header
+
+        # Ignore Columns
+        # If we want to exclude a column from the JSON output
+        # How can that be done?
 
         # First apply default options.
         plugin_settings = DEFAULT_OPTIONS.copy()
         plugin_settings.update(setting)
         setting = plugin_settings
+
+        # how to pase additional options
+        logger.debug("Camelot rule_options 1!!*%s*", setting["camelot_rules_json"])
+        rule_options = ""
+        rule_options = json.loads(setting["camelot_rules_json"]) #rule.
+        logger.debug("Camelot rule_options *%s*", rule_options)
+        # ok so now is passing json
 
         start = re.search(setting["start"], content)
         end = re.search(setting["end"], content)
@@ -89,26 +127,83 @@ def extract(self, content, output, invoicefile):
             )
             continue
 
-    tables = camelot.read_pdf(invoicefile, strip_text=currency_symbol)
+    tables = camelot.read_pdf(invoicefile) # , strip_text=currency_symbol
+
+
+    logger.debug(self) # write to output for testing
 
     logger.debug("Camelot tables %s", tables)
     df = tables[0].df
     # camelot_json = tables[0].to_json
     # logger.debug("Camelot json result %s", camelot_json)
 
+    #######################
+    # Replace header rows #
+    #######################
     # we got to replace the header
-    # new_header = df.iloc[0] #grab the first row for the header
-    # df = df[1:] #take the data less the header row
-    # df.columns = new_header #set the header row as the df header
+    #new_header = df.iloc[0] #grab the first row for the header
+    # df = df[1:] #take the data less the header row gives copy error
 
-    # alternative
-    # df.rename(columns=df.iloc[0]).drop(df.index[0])
-    df, df.columns = df[1:] , df.iloc[0]
+
+    # set first row as column name
+    df.rename(columns=df.iloc[0], inplace = True) # .drop(df.index[0], inplace = True)
+    # drop the first row from the dataset
+    df.drop(df.index[0], inplace = True)
+
+    # next step, rename the columns
+    df.rename(columns={'Discount' : 'Bill'}, inplace = True)
+
+    # When 2 similar named columns are passed, an error occurs b/c/ of not unique key constraint.
+    # But in this use case, we are not in control which clumn names are on the pdf.
+    # better solution is to assign a custom column name. Leave the index with an integer in tact
+    # when converting to json, use the custom column as key instead of the index.
+    # in that way, the user has the ability to replace the "double" key, before writing to json 
 
     logger.debug("Camelot df\n%s", df)
+    # testd = df.Amount.values.dtype
+    # logger.debug("testd is !!!! %s", testd) # return object
+
+    # export df
+    # df.to_csv("result.csv")
+
+
+    #######################
+    # Cleanup currency rows header rows #
+    #######################
+# def clean_currency_field():
+    # remove all non numbers from the Amount field
+    element = "Amount" # target element in output
+    amounttype = df.loc[:, 'Amount'].dtype
+    print(f"amounttype (clean alt)  is !!!! {amounttype}") # return object
+
+
+    if "," in self.options["decimal_separator"]:
+        # replace , with .
+        df.loc[:, (element)] = np.char.replace(df.loc[:, (element)].values.astype(str), ',','.')
+    df.loc[:, (element)] = [numbers(x) for x in df.loc[:, (element)]]
+    df.loc[:, (element)] = df.loc[:, (element)].replace('', np.nan).astype(float)
+
+    # Check the returned dtype
+    testd = df.loc[:, (element)].dtype
+    logger.debug(f"{element} dtype is {testd} !!!!") # return object
+
+
+    logger.debug(f"Camelot df after processing \n{df}")
+
+    #######################
+    # Cleanup currency rows header rows #
+    #######################
+    date_element = "Amount" # target element in output
+
+
+    #######################
+    # DATAFRAME TO JSON   #
+    #######################
+
     result = df.to_json(orient="records", force_ascii=False) # make orient configurable
     logger.debug("Pandas dataframe to json result\n%s", result)
 
+    # for debugging write json to logger
     parsed = json.loads(result)
     # json.dumps(parsed, indent=4)
     parsed = json.dumps(parsed,indent=4, sort_keys=True, ensure_ascii=False, separators=(',', ': ')).replace('\\n','\n')
@@ -116,43 +211,17 @@ def extract(self, content, output, invoicefile):
 
     # logger.debug("json dumps result %s", parsed)
 
+
+    # append the result to the output
     output["camelot"] = result
 
-    # tables.export('')
-    """
-    tables[0].to_csv('foo.csv')
-    # to_json, to_excel, to_html, to_markdown, to_sqlite
-    """
-    # or tables[0].df
-    """
+def numbers(element2):
 
-    raw_text = ""
-    raw_text = raw_text.encode(encoding='UTF-8')
-    with pdfplumber.open(path, laparams={"detect_vertical": True}) as pdf:
-        pages = []
-        for pdf_page in pdf.pages:
-            pages.append(
-                pdf_page.extract_text(
-                    layout=True, use_text_flow=True,
-                    x_tolerance=6,
-                    y_tolerance=4,
-                    keep_blank_chars=True
-                )
-                # y_tolerance=6, dirty Fix for html table problem
-            )
-        res = {
-            "all": "\n\n".join(pages),
-            "first": pages and pages[0] or "",
-        }
-    logger.debug("Text extraction made with pdfplumber")
-
-    raw_text = res_to_raw_text(res)
-    return raw_text.encode("utf-8")
-"""
+    return "".join(filter(is_number, element2)).replace(',','.') # .join(filter(str.isnumeric, element2))
 
 
-def res_to_raw_text(res):
-    # we need to convert result to raw text:
-    raw_text_dict = res
-    raw_text = (raw_text_dict["first"] or raw_text_dict["all"])
-    return raw_text
+def is_number(element2):
+    # logger.debug(f"Element2 of is number is !!!! *{element2}* ")
+    list_of_vowels = ['.', '-', ',']
+    if element2 in list_of_vowels or element2.isnumeric():
+        return True
