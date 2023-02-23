e9d64c135e68e2f0afc792f7e400d1386cd7ab29
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2023-02-23 01:06:02 +0100

    update ocrmypdf py

--- src/invoice2data/input/ocrmypdf.py
+++ src/invoice2data/input/ocrmypdf.py
@@ -1,16 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import tempfile
 
-from subprocess import Popen, PIPE, STDOUT, CalledProcessError, TimeoutExpired
-import shutil
+from pathlib import Path
+
+from . import pdftotext
 
 logger = logging.getLogger(__name__)
 
 
-def to_text(path: str, area_details: dict = None):
-    
-    """Parse PDF files with ocrmypdf for OCR.
+def have_ocrmypdf():
+    try:
+        import ocrmypdf  # noqa: F401
+    except ImportError:
+        return False
+    return True
+
+
+# Default options redo-ocr
+# to act as a fallback when pdftotext fails
+OPTIONS_DEFAULT = {
+    "redo_ocr": True,
+    "optimize": 0,
+    "output_type": "pdf",
+    "fast_web_view": 0,
+}
+
+
+def to_text(path, area_details: dict = None, input_reader_config: dict = {}):
+
+    """
+    Pre-process PDF files with ocrmypdf.
+    Before sending them to the pdftotext parser.
 
     Before usage make sure you have the dependencies installed.
 
@@ -26,62 +48,99 @@ def to_text(path: str, area_details: dict = None):
 
     """
 
+    if not have_ocrmypdf():
+        logger.warning("Cannot import ocrmypdf")
+        return b''
+
+    logger.debug("input_reader_config received from main are, *%s*", input_reader_config)
+
+    pre_proc_output = pre_process_pdf(path, pre_conf=input_reader_config)
+
+    if pre_proc_output:
+        extracted_str = pdftotext.to_text(pre_proc_output, area_details)
+    else:
+        return b''
+
+    return extracted_str
+
+
+def pre_process_pdf(path, pre_conf: dict = None):
+
+    """
+    Pre-process PDF files with ocrmypdf.
+    Before sending them to the pdftotext parser.
+
+    Before usage make sure you have the dependencies installed.
+
+    Parameters
+    ----------
+    path : str
+        path of electronic invoice in PDF format
+
+    Returns
+    -------
+    extracted_str : str
+        returns extracted text from pdf
+
+    Notes
+    -------
+    *output_file* can be same as input same to overwrite it.
+
+    For all the available options of ocrmypdf refer to:
+    https://ocrmypdf.readthedocs.io/en/latest/api.html#reference
+
+    Advanced example using custom settings for ocrmypdf and unpaper submodule
+    control of unpaper settings only works with clean-final or clean
+
+    unpaper_args =  "--pre-rotate -90,"
+
+    pre_conf = {
+    "output_file": "test_unpaper_rotate.pdf"
+    "clean-final": True,
+    "redo_ocr": False,
+    "force-ocr": True,
+    "unpaper_args": unpaper_args,
+    }
+
+    """
+
     try:
         import ocrmypdf
     except ImportError:
         logger.warning("Cannot import ocrmypdf")
 
-    timeout = 180
+    ocrmypdf_conf = OPTIONS_DEFAULT.copy()
 
-    # ocrmypdf.configure_logging(verbosity: Verbosity, *, progress_bar_friendly: bool = True, manage_root_logger: bool = False, plugin_manager=None)
-    # ocrmypdf.ocr('input.pdf', 'output.pdf', deskew=True)
-    logger.debug("Text extraction made with ocrmypdf")
+    # Merge the called arguments with defaults
+    ocrmypdf_conf.update(pre_conf)
+    logger.debug("ocrmypdf config settings are: *%s*", ocrmypdf_conf)
 
-    myocr_cmd = [
-        "ocrmypdf",
-        path,
-        "-",
-        "--redo-ocr"
-    ]
+    # Set output_file to temp folder, if it's not specified.
+    if "output_file" not in ocrmypdf_conf.keys():
+        inputfile = Path(path)
+        filename = inputfile.name
 
-    try:
-        p1 = Popen(myocr_cmd, stdout=PIPE)
-    except TimeoutExpired:
-        p1.kill()
-        logger.warning("ocrmypdf took too long - skipping")
-        return False
+        TMP_FOLDER = str(tempfile.gettempdir()) + "/"
+        ocrmypdf_conf["output_file"] = TMP_FOLDER + filename
+        logger.debug("no output_file specified, using temp file *%s*", ocrmypdf_conf["output_file"])
 
-    if shutil.which('pdftotext'):
-        cmd = ["pdftotext", "-layout", "-enc", "UTF-8"]
-        if area_details is not None:
-            # An area was specified
-            # Validate the required keys were provided
-            assert 'f' in area_details, 'Area r details missing'
-            assert 'l' in area_details, 'Area r details missing'
-            assert 'r' in area_details, 'Area r details missing'
-            assert 'x' in area_details, 'Area x details missing'
-            assert 'y' in area_details, 'Area y details missing'
-            assert 'W' in area_details, 'Area W details missing'
-            assert 'H' in area_details, 'Area H details missing'
-            # Convert all of the values to strings
-            for key in area_details.keys():
-                area_details[key] = str(area_details[key])
-            cmd += [
-                '-f', area_details['f'],
-                '-l', area_details['l'],
-                '-r', area_details['r'],
-                '-x', area_details['x'],
-                '-y', area_details['y'],
-                '-W', area_details['W'],
-                '-H', area_details['H'],
-            ]
-        cmd += ["-", "-"]
-        # Run the extraction
-        p2 = Popen(cmd, stdin=p1.stdout, stdout=PIPE)
-    try:
-        out, err = p2.communicate(timeout=timeout)
-        extracted_str = out
-    except TimeoutExpired:
-        p2.kill()
-        logger.warning("pdftotext took too long - skipping")
-    return extracted_str
+    logger.debug("OPTIONS !!!!, *%s*", ocrmypdf_conf)
+
+    # try to silence the large amount of debug loggs
+    logging.getLogger("ocrmypdf").setLevel(logging.WARNING)
+    logging.getLogger("pdfminer").setLevel(logging.WARNING)
+    logging.getLogger("ocrmypdf._pipeline").setLevel(logging.WARNING)
+    logging.getLogger("PIL.PngImagePlugin").setLevel(logging.WARNING)
+    logging.getLogger("ocrmypdf.helpers").setLevel(logging.WARNING)
+
+    exit_code = ocrmypdf.ocr(path, **ocrmypdf_conf)
+    if exit_code == 0:
+        logger.info("Text extraction made with ocrmypdf")
+    else:
+        logger.warning("ocrmypdf failed, stop the processing of this file")
+        return None
+
+    pre_proc_output = ocrmypdf_conf["output_file"]
+    logger.debug("ocrmypdf was called with output file!!!!, *%s*", pre_proc_output)
+
+    return pre_proc_output
