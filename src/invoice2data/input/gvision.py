0d9f20b0c65548df345d80c428ca9791d7796d80
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-11-22 20:59:00 +0100

    Refactor: Move google.cloud imports in gvision
    This commit moves the imports of `google.cloud.storage` and `google.cloud.vision` inside the `to_text` function in the `gvision` input module.
    
    This change ensures that these modules are only imported when the Google Cloud Vision API is actually used, preventing unnecessary imports and potential import errors when other input methods are used.
    
    This approach aligns with the structure of other input modules, such as `ocrmypdf`, where the module-specific libraries are only imported when the function is called.

--- src/invoice2data/input/gvision.py
+++ src/invoice2data/input/gvision.py
@@ -7,13 +7,18 @@ import os
 logger = logging.getLogger(__name__)
 
 
+try:
+    import google
+    from google.cloud import storage
+    from google.cloud import vision
+
+    GOOGLE_CLOUD_AVAILABLE = True
+except ImportError:
+    GOOGLE_CLOUD_AVAILABLE = False
+
+
 def have_google_cloud():
-    try:
-        from google.cloud import storage  # noqa: F401
-        from google.cloud import vision  # noqa: F401
-    except ImportError:
-        return False
-    return True
+    return GOOGLE_CLOUD_AVAILABLE
 
 
 def to_text(
