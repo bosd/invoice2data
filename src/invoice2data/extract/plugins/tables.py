586a772319da16fdd64385cc5fb60bf9f99b35d8
bosd <emiel.vanbokhoven@obs-solutions.com>
2025-08-13 01:24:23 +0200

    Update tables.py

--- src/invoice2data/extract/plugins/tables.py
+++ src/invoice2data/extract/plugins/tables.py
@@ -204,12 +204,6 @@ def _process_table_line(  # noqa: C901
         elif field in types:
             value = self.coerce_type(value, types[field])
         elif table.get("fields"):
-            # Writing templates is hard. So we also support the following format
-            # In case someone mixup syntax
-            # fields:
-            #    example_field:
-            #      type: fl...
-            #      group: sum
             field_set = table["fields"].get(field, {})
             if "type" in field_set:
                 value = self.coerce_type(value, field_set.get("type"))
